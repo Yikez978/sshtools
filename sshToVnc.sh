6624fb4e618ee7591cac1afb8aefaa96cdacc037
swys <abacus822@gmail.com>
2013-09-14 18:00:23 -0400

    intial commit...added questions to script to start setting core variables ip/port/username

---
+++ sshToVnc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "**************************"
+echo "       SSH to VNC"
+echo "**************************"
+dport=5140
+duser=$(whoami)
+read -p "Enter remote IP address :"  ip
+echo "You have entered $ip"
+read -p "Enter remote PORT to connect to : [press enter for port $dport] :" port
+port=${port:-$dport}
+echo "You have entered $port"
+read -p "Enter the username you wish to connect as : [press enter to use $duser] :"  user
+user=${user:-$duser}
+echo "Attempting to ssh to $ip on port $port with as username $user"
+exit 0
