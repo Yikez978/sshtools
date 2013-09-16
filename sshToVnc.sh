c2c32d8dde23bf712a58f01e11411c0fc2d7efeb
swys <abacus822@gmail.com>
2013-09-16 14:29:31 -0400

    added process watcher after VNC is launched that will kill ssh once the VNC process is no longer running...clean up after we are done

--- sshToVnc.sh
+++ sshToVnc.sh
@@ -3,9 +3,11 @@
 echo "**************************"
 echo "       SSH to VNC"
 echo "**************************"
+dip=192.168.1.35
 dport=5140
 duser=$(whoami)
-read -p "Enter remote IP address :"  ip
+read -p "Enter remote IP address : [press enter for $dip] : "  ip
+ip=${ip:-$dip}
 echo "You have entered $ip"
 read -p "Enter remote PORT to connect to : [press enter for port $dport] :" port
 port=${port:-$dport}
@@ -13,9 +15,22 @@ echo "You have entered $port"
 read -p "Enter the username you wish to connect as : [press enter to use $duser] :"  user
 user=${user:-$duser}
 echo "Attempting to ssh to $ip on port $port with as username $user"
-(
 ssh -p $port -N -L 5905:127.0.0.1:5900 $user@$ip &
-sleep 3
+sleep 1
 open vnc://127.0.0.1:5905
-)
-exit 0
+sleep 2
+screenPID=$(ps -A | grep 'Screen Sharing' | grep -v grep | awk '{print $1}')
+sshPID=$(lsof | grep "5905" | grep -v IPv6 | awk '{print $2}')
+echo "The Screen Sharing PID is : $screenPID"
+kill -0 $screenPID >/dev/null 2>&1
+result=$?
+while [ $result -ne 1 ]; do
+    sleep 1
+    kill -0 $screenPID >/dev/null 2>&1
+    result=$?
+done
+echo "Now is the time to kill to process?"
+kill $sshPID
+exitCode=$?
+echo "Exit Code : $exitCode"
+exit $exitCode
