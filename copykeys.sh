5bc25c16aeccedee3931212d1887b692a940ce32
swys <abacus822@gmail.com>
2014-03-07 23:48:26 -0500

    instead of making a temp file and using SCP to overwrite existing authorized_keys folder, changed method to just append the existing authorized keys folder. This will allow the script to be run from multile clients on same server without deleting previous key setup every time you add a new key.

--- copykeys.sh
+++ copykeys.sh
@@ -15,25 +15,18 @@ port=${port:-$dport}
 read -p "Enter the username you wish to connect as : [press enter to use $duser] :" user
 user=${user:-$duser}
 
-#copy public key to file
-cat ~/.ssh/id_rsa.pub >> authorized_keys
-chmod 644 authorized_keys
+echo "going to append public key to port $port at $user@$ip"
 
-echo "going to copy to port $port at $user@$ip"
-#send public key file to remote system
-scp -P $port authorized_keys $user@$ip:~/.ssh/
+#append public key file to remote system
+cat ~/.ssh/id_rsa.pub | ssh $user@$ip -p $port 'umask 077; cat >>.ssh/authorized_keys'
 
 #exit with same exit code as above command
 exitCode=$?
 if [[ $exitCode -gt 0 ]]; then
   echo "Something went terribly wrong!!!!"
-  echo "Cleaning up files..."
-  rm authorized_keys
   echo "Exit Code : $exitCode"
   exit $exitCode
 else
   echo "Successfully copied public key to remote machine!!!!"
-  echo "Cleaning up files..."
-  rm authorized_keys
   exit $?
 fi
